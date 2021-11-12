bbd8cbe189e4adb600d0a26cae147c88a7a09d11
bhavikmalhotra <bhavik.malhotra52@gmail.com>
2021-11-12 15:15:58 -0500

    First commit

---
+++ challenge/sqli.php
@@ -0,0 +1,48 @@
+<html >
+<head>
+<title>Less-1 **Error Based- SQLI**</title>
+</head>
+
+<p><strong>Welcome</strong>Please input the id as parameter with numeric value</p>
+
+<?php
+//including the Mysql connect parameters.
+include("../mysql/connection.php");
+error_reporting(0);
+// take the variables 
+if(isset($_GET['id']))
+{
+$id=$_GET['id'];
+//logging the connection parameters to a file for analysis.
+$fp=fopen('result.txt','a');
+fwrite($fp,'ID:'.$id."\n");
+fclose($fp);
+
+// connectivity 
+
+
+$sql="SELECT * FROM users WHERE id='$id' LIMIT 0,1";
+$result=mysql_query($sql);
+$row = mysql_fetch_array($result);
+
+        if($row)
+        {
+        echo "<font size='5' color= '#99FF00'>";
+        echo 'Your Login name:'. $row['username'];
+        echo "<br>";
+        echo 'Your Password:' .$row['password'];
+        echo "</font>";
+        }
+        else 
+        {
+        echo '<font color= "#FFFF00">';
+        print_r(mysql_error());
+        echo "</font>";  
+        }
+}
+        else { echo "Please input the ID as parameter with numeric value";}
+
+?>
+</font> </div></br></br></br><center>
+</body>
+</html>
